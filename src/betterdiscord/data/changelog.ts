353fb655165e0d43323d90501cd3783f5993f9b5
Robert Delaney <Doggybootsy@gmail.com>
2026-04-01 00:02:19 -0700

    Bump to 1.13.11

--- src/betterdiscord/data/changelog.ts
+++ src/betterdiscord/data/changelog.ts
@@ -8,14 +8,29 @@ export default {
     // https://youtu.be/BZq1eb9d0HI?si=67V2eArlF4atnGnz
     // video: "https://www.youtube.com/embed/BZq1eb9d0HI?si=67V2eArlF4atnGnz&vq=hd720p&hd=1&rel=0&showinfo=0&mute=0&loop=1&autohide=1",
     // banner: "https://i.imgur.com/wuh5yMK.png",
-    blurb: "Hotfix to squash some bugs.",
+    blurb: "All around improvements.",
     changes: [
         {
-            title: "Hot Fix",
+            title: "Dropdown Improvements",
+            type: "improved",
+            items: [
+                "Dropdowns now close after selecting an option",
+                "Hides the scrollbar when its not needed"
+            ]
+        },
+        {
+            title: "Fix for Developers",
             type: "fixed",
             items: [
-                "Fixed crashing when trying to view BetterDiscord's custom setting tabs"
+                "Fix for lazy listeners firing before class modules get interpolated"
             ]
-        }
+        },
+        {
+            title: "Misc. Improvements",
+            type: "improved",
+            items: [
+                "Improved the way other client mods are handled to prevent conflicts",
+            ]
+        },
     ]
 } as ChangelogProps;
